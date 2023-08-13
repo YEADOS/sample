0850dea8f1ac9633a01beff4a54316fdcbddd6db
YEADOS <nicholasyeadon@gmail.com>
2023-08-13 19:38:56 +1000

    Laptop

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+int testFunction() {
+    return 5; 
+}
+
 int main() {
-    std::cout << "Hello World";
+    std::cout << "Hello World" << std::endl;
+    std::cout << testFunction(); 
     return 0; 
 }
