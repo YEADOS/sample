4d00a3d0fb4f49acd667745a08c09d86c65eeaec
YEADOS <nicholasyeadon@gmail.com>
2023-08-28 22:31:38 +1000

    yo

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <string> 
+
+std::string yo(std::string name) {
+    std::string s = "Get Good ";
+    return s + name;
+}
 
 int testFunction() {
     return 5; 
@@ -7,5 +13,6 @@ int testFunction() {
 int main() {
     std::cout << "Hello World" << std::endl;
     std::cout << testFunction(); 
+    std::cout << yo("Bob");
     return 0; 
 }
