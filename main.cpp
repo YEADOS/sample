699543a9c5f2a280889ef1bb82f0f701a721b59c
YEADOS <nicholasyeadon@gmail.com>
2023-08-29 22:26:10 +1000

    linux

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 
 using namespace std; 
 
+void linux() {
+    cout << "This is linux mint" << "\n"; 
+    
+}
+
 string getGood(string name) {
     string s = "Get Good ";
     return s + name;
@@ -18,7 +23,8 @@ int main() {
     string input;
     cin >> input; 
     cout << "Hello World" << endl;
-    cout << testFunction() << endl; 
+    cout << testFunction() << endl;
+    linux();  
     cout << getGood(input);
     return 0; 
 }
